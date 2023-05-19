26d86c38827dae5be7196684eec896bd48c95f70
George Polizoidis <georgepol01@yahoo.com>
2023-05-19 21:19:12 +0300

    merge

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -25,6 +25,40 @@ function showMenu() {
 
 
 //CATEGORY FUNCTION
-document.getElementById('categoryButons').addEventListener('click', function () {
-    window.location.href = '/Home/Sign_Up';
-});
+function fetchData(buttonId) {
+    // Get the category based on the buttonId
+    const category = getCategoryFromButtonId(buttonId);
+
+    // Fetch data from the server based on the category
+    fetch(`/api/data?category=${category}`)
+        .then((response) => response.json())
+        .then((data) => {
+            // Update the UI with the fetched data
+            updateUI(data);
+        })
+        .catch((error) => {
+            console.error('Error fetching data:', error);
+        });
+}
+
+function getCategoryFromButtonId(buttonId) {
+    // Return the appropriate category based on the buttonId
+    // ...
+}
+
+//DISPLAY DATA
+function updateUI(data) {
+    // Get the element where you want to display the data
+    const dataContainer = document.getElementById('dataContainer');
+
+    // Clear the previous content
+    dataContainer.innerHTML = '';
+
+    // Iterate through the fetched data and create new elements to display it
+    data.forEach((item) => {
+        const dataElement = document.createElement('div');
+        dataElement.textContent = item.name; // Replace 'name' with the appropriate property from your data model
+        dataContainer.appendChild(dataElement);
+    });
+}
+
